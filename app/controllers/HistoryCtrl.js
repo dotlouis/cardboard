d4ef27758f197d32b23fa75acab689cec546e001
dotlouis <1891109+dotlouis@users.noreply.github.com>
2015-02-18 12:31:31 +0100

    History Card
    History card shows 5 last visited website and the time of the visit.
    Closes #17

---
+++ app/controllers/HistoryCtrl.js
@@ -0,0 +1,13 @@
+angular.module('cardboard.controllers')
+
+.controller('HistoryCtrl', ['$scope','ChromeFactory', function($scope, Chrome){
+    $scope.maxHistory = 5;
+
+    Chrome.history.searchAsync({
+        text: "", // empty for all pages
+        maxResults: $scope.maxHistory
+    }).then(function(history){
+        $scope.history = history;
+        $scope.$apply();
+    });
+}]);
