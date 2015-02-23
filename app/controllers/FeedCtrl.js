f13385f62329ae26000afa9f2bd3b0016ca635ab
virtual-dev <louis1.lafont@gmail.com>
2015-02-23 01:07:10 +0100

    New Fab behaviour + moved cards to feedCtrl
    Before:
    on mouseenter the fab would show its children and on mouseleave it
    would make them transparent.
    This was not ideal because the tooltips would show up and by hovering
    on the empty area over the fab it would bring back the children.
    
    Now:
    fab requires a mouse click to show its children.
    on mouseleave the children are display: none which means they don't
    block what's behind anymore. (animation are preserved)
    
    So there is a bit of jQuery code but couldn't go without it with the
    materializecss framework.
    
    Fab and Cards stuff are moved to the FeedCtrl for a more modulable
    code. (ligthen the rootctrl)

--- app/controllers/FeedCtrl.js
+++ app/controllers/FeedCtrl.js
@@ -2,14 +2,142 @@ angular.module('cardboard.controllers')
 
 .controller('FeedCtrl',['$scope', function($scope){
 
-    $scope.dailyMsg = $scope.welcomeMessages[Math.floor((Math.random() * $scope.welcomeMessages.length))];
-
     $scope.isDrag = false;
+
+    $scope.settings.then(function(storage){
+        var settings = storage[0];
+
+        // init welcome messages
+        $scope.dailyMsg = settings.welcomeMessages[Math.floor((Math.random() * settings.welcomeMessages.length))];
+
+        // check each card permissions;
+        return Promise.map(settings.cards, function(card){
+            return Chrome.permissions.check(card.permissions)
+            .then(function(granted){
+                // Disable the card if not granted AND enabled
+                card.enabled = (granted && card.enabled);
+                return card;
+            });
+        });
+    })
+    .then(function(cards){
+        $scope.$apply(function(){
+            $scope.cards = cards;
+        });
+        checkCardsEnabled();
+        checkCardsDisabled();
+    });
+
+
     // see http://packery.metafizzy.co/faq.html#order-after-drag
     // and http://packery.metafizzy.co/methods.html#packery-data
     // var pckryElement = document.querySelector('#feed > div');
     // console.log(pckryElement);
     // var pckry = Packery.data(pckryElement);
     // console.log(pckry.getItemElements());
+
+    $scope.toggle = function(card, on){
+        var self = this;
+        if(typeof on === "undefined")
+            on = card.enabled;
+
+        if(on)
+            Chrome.permissions.request(card.permissions)
+            .then(function(){
+                // Granted
+                $scope.$apply(function(){
+                    card.enabled = true;
+                    // We know all cards are not disabled, we just enabled one
+                    $scope.allCardsDisabled = false;
+                });
+                checkCardsEnabled();
+                Chrome.storage.setAsync({'cards':$scope.cards});
+            })
+            .catch(function(){
+                // Denied, we don't enable the card
+                $scope.$apply(function(){
+                    card.enabled = false;
+                    // We know all cards are not enabled, we just disabled one
+                    $scope.allCardsEnabled = false;
+                });
+                checkCardsDisabled();
+                toast("Card needs permission to run", 4000);
+            });
+        else
+            Chrome.permissions.revoke(card.permissions)
+            .then(function(){
+                $scope.$apply(function(){
+                    card.enabled = false;
+                    // We know all cards are not enabled, we just disabled one
+                    $scope.allCardsEnabled = false;
+                });
+                checkCardsDisabled();
+                Chrome.storage.setAsync({'cards':$scope.cards});
+            });
+    };
+
+    function checkCardsEnabled(){
+        for(var i=0; i<$scope.cards.length; i++){
+            if(!$scope.cards[i].enabled){
+                $scope.$apply(function(){$scope.allCardsEnabled = false;});
+                break;
+            }
+            else if(i == $scope.cards.length-1)
+                $scope.$apply(function(){$scope.allCardsEnabled = true;});
+        }
+    }
+    function checkCardsDisabled(){
+        for(var i=0; i<$scope.cards.length; i++){
+            if($scope.cards[i].enabled){
+                $scope.$apply(function(){$scope.allCardsDisabled = false;});
+                break;
+            }
+            else if(i == $scope.cards.length-1)
+                $scope.$apply(function(){$scope.allCardsDisabled = true;});
+        }
+    }
+
+    /********* FAB STUFF **********/
+
+    var fab = $('.fixed-action-btn');
+    fab.off( "mouseenter mouseleave" ); // Disable FAB on hover
+
+    $('.fixed-action-btn').mouseleave(fabOff);
+
+    $scope.triggerFab = function(){
+        if($scope.fab)
+            fabOff();
+        else
+            fabOn();
+    };
+
+    function fabOn(){
+        var time = 0;
+        fab.children('ul').css('display','block');
+        fab.find('ul a.btn-floating').reverse().each(function () {
+            $(this).velocity(
+                { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+                { duration: 100, delay: time });
+            time += 40;
+        });
+        $scope.fab = true;
+    }
+
+    function fabOff(){
+        var time = 0;
+        fab.find('ul a.btn-floating').velocity("stop", true);
+        fab.find('ul a.btn-floating').velocity(
+            { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+            {
+                duration: 100,
+                complete: function(){
+                    setTimeout(function () {
+                        fab.children('ul').css('display','none');
+                    }, 50);
+                }
+            });
+
+        $scope.fab = false;
+    }
 
 }]);
