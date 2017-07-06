9a4f1282b4d7cb2a250f0e928d95cfd72fc55ce6
Leigende <alexisr245@gmail.com>
2017-07-06 16:16:13 +0200

    Don't download trends if not enabled

--- src/controllers/RootCtrl.js
+++ src/controllers/RootCtrl.js
@@ -26,7 +26,8 @@ angular.module('cardboard.controllers')
 
                 // Init trends
                 $scope.trends = settings.sync.trends;
-                $scope.trends.data = Trends.get();
+                if ($scope.trends.enabled)
+                    $scope.trends.data = Trends.get();
                 $scope.trends.start = true
 
                 // If local background (dataURl) we get it from cache
@@ -77,6 +78,48 @@ angular.module('cardboard.controllers')
             }
 
             /********* USEFUL STUFF **********/
+
+            $scope.xmlToJson = function (xml) {
+
+                // Create the return object
+                var obj = {};
+
+                if (xml.nodeType == 1) { // element
+                    // do attributes
+                    if (xml.attributes.length > 0) {
+                        obj["@attributes"] = {};
+                        for (var j = 0; j < xml.attributes.length; j++) {
+                            var attribute = xml.attributes.item(j);
+                            obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+                        }
+                    }
+                } else if (xml.nodeType == 3) { // text
+                    obj = xml.nodeValue;
+                }
+
+                // do children
+                // If just one text node inside
+                if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
+                    obj = xml.childNodes[0].nodeValue;
+                }
+                else if (xml.hasChildNodes()) {
+                    for (var i = 0; i < xml.childNodes.length; i++) {
+                        var item = xml.childNodes.item(i);
+                        var nodeName = item.nodeName;
+                        if (typeof (obj[nodeName]) == "undefined") {
+                            obj[nodeName] = xmlToJson(item);
+                        } else {
+                            if (typeof (obj[nodeName].push) == "undefined") {
+                                var old = obj[nodeName];
+                                obj[nodeName] = [];
+                                obj[nodeName].push(old);
+                            }
+                            obj[nodeName].push(xmlToJson(item));
+                        }
+                    }
+                }
+                return obj;
+            };
 
             $scope.getFavicon = function (url) {
                 const regex = /(chrome:\/\/|view-source:)/ig;
